6abc8cd7daf9152894784154e8c2796c58e93a5d
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-01-06 00:39:38 +0200

    read and show image according to query params provided

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express, { Application, Request, Response } from 'express'
+import routes from './routes/index'
+import { promises as fsPromises } from 'fs'
 
 const app: Application = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
+app.use(express.static('public'))
+
+app.use('/api', routes)
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Home Page')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
