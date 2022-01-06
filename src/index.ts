d393645ca6065dac4800ac8179a8e358c19f540d
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-01-06 13:48:47 +0200

    attached sharp library to process images

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from 'express'
-import routes from './routes/index'
-import { promises as fsPromises } from 'fs'
+import express, { Application, Request, Response } from "express"
+import routes from "./routes/index"
+import path from "path"
 
 const app: Application = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static(`${__dirname}/public`))
 
-app.use('/api', routes)
+app.use("/api", routes)
 
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Home Page')
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Home Page")
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
