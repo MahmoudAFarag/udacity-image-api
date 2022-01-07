05cf978d92562ed36de12fdeb547513d12cbbab3
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-01-07 23:48:30 +0200

    api ready for testing

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,10 @@ import routes from "./routes/index"
 const app: Application = express()
 const port = 3000
 
-// Add static folder to serve images
-app.use(express.static(`${__dirname}/public`))
+// morgan middleware
 app.use(morgan("tiny"))
 
 // Import api route
 app.use("/api", routes)
-
-app.get("/", async (req: Request, res: Response) => {
-  res.send("Home Page")
-})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
